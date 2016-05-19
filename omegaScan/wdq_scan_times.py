abe806accf21cbecaf41c18fb472f5c7d0002566
Joshua Smith <joshua.smith@ligo.org>
2016-05-19 13:53:09 -0700

    Added code to omega scan a list of times.

---
+++ omegaScan/wdq_scan_times.py
@@ -0,0 +1,22 @@
+# Scan some times using wdq
+import os
+import numpy as np
+
+ifo='H1'
+filen = 'nikhil_tomte.txt';
+times = np.loadtxt(filen,float)
+#print times
+
+wdq = '/home/detchar/bin/wdqER'
+options = '-a accounting_group=ligo.prod.o1.detchar.user_req.omegascan'
+outdir = '/home/jrsmith/public_html/omegaScans/tomtes/'
+
+for time in times:
+	stime =  "%f" % (time) # convert times to a string
+	command = "nohup condor_run " + wdq + " " + options + " " + ifo \
+	+ " " + stime + " " + outdir + stime + "/" \
+	+ " < /dev/null &>/dev/null &"
+
+	print "Running wdq for ifo=" + ifo + " GPS=" + stime
+	#print command
+	os.system(command)

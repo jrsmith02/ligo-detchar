aa3802e66c4839d006d5a6a9feb247a53b2b348a
Joshua Smith <joshua.smith@ligo.org>
2019-01-03 17:37:57 -0800

    Added some input arg handling.

--- scattering/scatMon3.py
+++ scattering/scatMon3.py
@@ -8,6 +8,7 @@
 # Use timeseries dict to get all channels at once
 # Make plots using Q-transform rather than spectrogram to better visualize 
 # the fringes. 
+# Load position channels in as a text file. 
 
 # tell matplotlib to use a non-interactive backend for generating png images
 from matplotlib import use
@@ -22,48 +23,46 @@ from matplotlib.image import NonUniformImage
 import matplotlib.mlab as mlab
 from os import makedirs, path
 import string 
+import argparse
+from gwpy.time import to_gps
 
-# read input argumentsi (update this to use argparse)
-if len(sys.argv) == 6:
-	ifo = sys.argv[1] # H1 or L1
-	start_time = int(sys.argv[2]) # GPS start time
-	startutc = str(from_gps(start_time)) # get UTC time
-	dur = int(sys.argv[3]) # duration in seconds
-	thresh = int(sys.argv[4]) # threshold frequency [Hz] (add as input argument later)
-	plotspec = int(sys.argv[5]) # 0 for no spectrogram, 1 for spectrogram
-	end_time = int(start_time) + int(dur)
-else:
-	print "Usage: python scatMon.py ifo start_time dur thresh plospec"
-	sys.exit(2)
+__author__ = 'Joshua Smith'
+__email__ = 'joshua.smith@ligo.org'
+__version__ = '0.0.1'
+prog = 'ScatMon3'
 
-### Set the channel that witnesses fringes (to plot specgram for) 
-#witness_base = "LSC-DARM_IN1_DQ"
-#witness_base = "GDS-CALIB_STRAIN"
-#witness_base = "LSC-MICH_IN1_DQ"
-#witness_base = '%s:ASC-Y_TR_A_NSUM_OUT_DQ' % ifo
-#witness_base = 'LSC-SRCL_IN1_DQ'
-#witness_base = "LSC-REFL_A_RF9_Q_ERR_DQ"
-#witness_base = "ASC-AS_A_RF45_Q_PIT_OUT_DQ" 
-#witness_base = "ASC-AS_B_RF36_Q_PIT_OUT_DQ"
-#witness_base = "OMC-LSC_SERVO_OUT_DQ"
-witness_base = "ASC-Y_TR_B_PIT_OUT_DQ"
+# command line options
+parser = argparse.ArgumentParser(description=__doc__, prog=prog)
 
-if plotspec==1:
-	witness_chan = ifo + ':' + witness_base
-	print witness_chan
-	# load timeseries for witness channel
-        witness=TimeSeries.fetch(witness_chan, start_time, start_time+dur, verbose=True)
-	if witness_base=="GDS-CALIB_STRAIN":
-		witness=witness.highpass(20,gpass=3) # highpass the witness data
-        elif witness_base=="LSC-DARM_IN1_DQ" or witness_base=="ASC-AS_B_RF45_I_PIT_OUT_DQ" or witness_base=="ASC-AS_B_RF36_Q_PIT_OUT_DQ" or witness_base=="LSC-MICH_IN1_DQ" or witness_base=="ASC-AS_A_RF45_Q_PIT_OUT_DQ":
-		witness=witness.highpass(15,gpass=3) # highpass the witness data
-	# Calculate DARM spectrogram 
-        secsPerFFT = .75 # Hz
-	overlap = 0.9 # fractional overlap
-        Fs = witness.sample_rate.value
-        NFFT = int(round(Fs*secsPerFFT))
-        noverlap = int(round(overlap*NFFT))
-        Pxx, freq, t, im = specgram(witness.value,NFFT=NFFT,Fs=witness.sample_rate.value,noverlap=noverlap,scale_by_freq='magnitude',detrend=mlab.detrend_linear,window=mlab.window_hanning)
+parser.add_argument('--start', type=to_gps, action='store', default=1230524574,
+                    help='Start time GPS or yyyy-mm-dd hh:mm:ss.mmm ')
+parser.add_argument('--end', type=to_gps, action='store', default=1230524584,
+                    help='End time GPS or yyyy-mm-dd hh:mm:ss.mmm ')
+parser.add_argument('-i', '--ifo', default='H1')
+parser.add_argument('-p', '--primary', default='ASC-Y_TR_B_PIT_OUT_DQ')
+
+args = parser.parse_args()
+
+start_time = args.start
+end_time = args.end
+ifo = args.ifo
+witness_base = args.primary
+
+witness_chan = ifo + ':' + witness_base
+print witness_chan
+
+# load timeseries for witness channel
+witness=TimeSeries.fetch(witness_chan, start_time, end_time, verbose=True)
+if witness_base=="GDS-CALIB_STRAIN":
+	witness=witness.highpass(20,gpass=3) # highpass the witness data
+
+# Calculate DARM spectrogram 
+secsPerFFT = .75 # Hz
+overlap = 0.9 # fractional overlap
+Fs = witness.sample_rate.value
+NFFT = int(round(Fs*secsPerFFT))
+noverlap = int(round(overlap*NFFT))
+Pxx, freq, t, im = specgram(witness.value,NFFT=NFFT,Fs=witness.sample_rate.value,noverlap=noverlap,scale_by_freq='magnitude',detrend=mlab.detrend_linear,window=mlab.window_hanning)
 
 ### Known SUS displacement w/r/t sus frame channels (in microns)
 position_chans = [\
@@ -107,7 +106,7 @@ position_chans = [\
 for channel in position_chans:
 	position_chan = '%s:%s' % (ifo,channel)
 	print position_chan
-	position=TimeSeries.fetch(position_chan, start_time, start_time+dur, verbose=True)
+	position=TimeSeries.fetch(position_chan, start_time, end_time, verbose=True)
 	position=position.detrend()
 	position=position.lowpass(5,gpass=3) # lowpass position channel
 	velocity = (position[1:]-position[:-1])*(position.sample_rate.value)
@@ -117,7 +116,8 @@ for channel in position_chans:
 	scatf2 = 2.0*scatf1
 	scatf3 = 3.0*scatf1
 	scatf4 = 4.0*scatf1
-	scatf5 = 5.0*scatf1
+	scatf8 = 8.0*scatf1
+	scatf16 = 16.0*scatf1
 	# print max scatter values and their times
 	print 'max scatter f1 = ' + str(max(scatf1)) + ' Hz'
 	tofmax = times[argmax(scatf2)]
@@ -127,39 +127,38 @@ for channel in position_chans:
 	fig = plt.figure(figsize=(12,12))
 	ax1 = fig.add_subplot(211)
 	# Plot Spectrogram
-	if plotspec==1:
-        	im1 = NonUniformImage(ax1, interpolation='bilinear',extent=(min(t),max(t),10,55),cmap='jet')
-        	im1.set_data(t,freq,20.0*log10(Pxx))
-        	if witness_base=="GDS-CALIB_STRAIN":
-			print "setting color limits for STRAIN"
-			im1.set_clim(-1000,-800)
-        	elif witness_base=="ASC-AS_B_RF45_Q_YAW_OUT_DQ" or witness_base=="ASC-AS_B_RF36_Q_PIT_OUT_DQ" or witness_base=="ASC-AS_A_RF45_Q_PIT_OUT_DQ" or witness_base=="LSC-MICH_IN1_DQ":
-			im1.set_clim(-200,20)
-		elif witness_base == "OMC-LSC_SERVO_OUT_DQ":
-			im1.set_clim(-240,-85)
-		ax1.images.append(im1)
-        	#cbar1 = fig.colorbar(im1)
-        	#cbar1.set_clim(-120,-40)
+        im1 = NonUniformImage(ax1, interpolation='bilinear',extent=(min(t),max(t),10,55),cmap='jet')
+        im1.set_data(t,freq,20.0*log10(Pxx))
+        if witness_base=="GDS-CALIB_STRAIN":
+		print "setting color limits for STRAIN"
+		im1.set_clim(-1000,-800)
+        #elif witness_base=="ASC-AS_B_RF45_Q_YAW_OUT_DQ" or witness_base=="ASC-AS_B_RF36_Q_PIT_OUT_DQ" or witness_base=="ASC-AS_A_RF45_Q_PIT_OUT_DQ" or witness_base=="LSC-MICH_IN1_DQ":
+	#	im1.set_clim(-200,20)
+	#elif witness_base == "OMC-LSC_SERVO_OUT_DQ":
+	im1.set_clim(-400,-220)
+	ax1.images.append(im1)
+        cbar1 = fig.colorbar(im1)
+        #cbar1.set_clim(-540,-240)
 	
 	# plot fringe prediction timeseries
 	#ax1.plot(times,scatf5, c='blue', linewidth='0.2', label='f5')
-	ax1.plot(times,scatf4, c='purple', linewidth='0.4', label='f4')
-	ax1.plot(times,scatf3, c='green', linewidth='0.4', label='f3')
-	ax1.plot(times,scatf2, c='blue', linewidth='0.4', label='f2')
-	ax1.plot(times,scatf1, c='black', linewidth='0.4', label='f1')
+	ax1.plot(times,scatf16, c='purple', linewidth='0.6', label='f16')
+	ax1.plot(times,scatf8, c='green', linewidth='0.6', label='f8')
+	ax1.plot(times,scatf4, c='blue', linewidth='0.6', label='f4')
+	ax1.plot(times,scatf2, c='black', linewidth='0.6', label='f2')
 	#if plotspec < 1 and dur <= 3600:
         #	ax1.plot(highscattimes, highscatf2, c='r', label='veto', marker='.',linestyle='')
 	ax1.legend( loc='upper right' )
-	if plotspec==1:
-		title = 'Specgram of ' + string.replace(witness_chan,'_','\_') + '\nScattering fringe frequency for ' + string.replace(position_chan,'_','\_')
-	else: 
-       		title = 'Scattering fringe frequency for ' + string.replace(position_chan,'_','\_')
+	title = 'Specgram of ' + string.replace(witness_chan,'_','\_') + '\nScattering fringe frequency for ' + string.replace(position_chan,'_','\_')
+       	#title = 'Scattering fringe frequency for ' + string.replace(position_chan,'_','\_')
 	# plt.title(title,fontsize=16)
         plt.yscale('linear')
 	plt.ylabel('Frequency [Hz]',fontsize=16)
-	xlab = 'Time [seconds] from ' + str(startutc) + ' UTC'
+	xlab = 'Time [seconds] from ' + str(args.start)
         plt.xlim(0,max(times))
-        plt.ylim(0,50)
+        plt.ylim(0,35)
+	plt.title(title)
+
 	ax2 = fig.add_subplot(212)
 	#print(len(position.data))
 	#print(position.data)
@@ -169,46 +168,12 @@ for channel in position_chans:
 	plt.ylabel('Position [um]',fontsize=16)
 
 	# save figure
-	dir = str(start_time) + '-' + str(dur) + '/'
+	dir = str(start_time) + '-' + str(end_time-start_time) + '/'
 	if not path.exists(dir):
 		makedirs(dir)
-	filename = dir + ifo + '-Fscatter-' + channel + '-' + str(start_time) + '-' + str(dur) + '.png'
+	filename = dir + ifo + '-Fscatter-' + channel + '-' + str(start_time) + '-' + str(end_time-start_time) + '.png'
 	savefig(filename)
 	plt.close(fig)
 
 quit()
-
-############### Below here is old code for making DQ flags, needs to be cleaned up
-
-if plotspec < 1:
-	# determine times when f2 above some threshold
-	idxhighscat = argwhere(scatf2>=thresh)
-	highscatf2 = scatf2[idxhighscat]
-	highscattimes = times[idxhighscat]
-	highscattimesgps = highscattimes+start_time
-
-	# save text file with values above threshold [GPS f2 index]
-	outdata = hstack((highscattimesgps,highscatf2,idxhighscat))
-	savetxt('%s-ALL-TIMES-SCATTER-GT%dHZ-%d-%d.txt' % (ifo,thresh,start_time,dur),outdata,fmt='%f %f %i')
-
-	# save segments XML file with segments (based off code from Duncan Macleod)
-	from math import (floor, ceil)
-	from gwpy.segments import (Segment, DataQualityFlag)
-
-	flag = '%s:DCH-SCATTERED_LIGHT_GT%dHZ:1' % (ifo,thresh) 
-	flag = DataQualityFlag(flag)
-	segs = []
-	append = segs.append
-
-	for gps in highscattimesgps:
-    		if len(segs) and gps in segs[-1]:
-        		continue
-    		seg = Segment(floor(gps), ceil(gps))
-    		append(seg)
-
-	flag.active = segs
-	flag.known = [Segment(start_time, end_time)]
-	flag.coalesce()
-	flag.write('%s-%s_%d-%d-%d.xml.gz' % (flag.ifo, flag.tag.replace('-', '_'), flag.version,start_time, dur))
-
-#EOF
+# EOF
